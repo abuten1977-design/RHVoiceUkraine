e4c7a2da8956fd635ed0f22b8b3f5b0e44542eb7
Andriy Butenko <abuten1977@gmail.com>
2026-03-10 20:50:28 +0100

    fix: Add Objective-C bridging header for RHVoiceEngine

---
+++ UkrainianVoicesApp/Extension/Bridge/UkrainianVoices-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  UkrainianVoices-Bridging-Header.h
+//  Bridge between Objective-C++ and Swift
+//
+
+#ifndef UkrainianVoices_Bridging_Header_h
+#define UkrainianVoices_Bridging_Header_h
+
+#import "RHVoiceEngine.h"
+
+#endif /* UkrainianVoices_Bridging_Header_h */
