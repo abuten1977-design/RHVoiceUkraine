711ee902c27607036bec6823b35ef0e8f98f3c50
Andriy Butenko <abuten1977@gmail.com>
2026-04-06 12:02:54 +0200

    Fix: add Sentry via project.yml SPM packages (not Extension/Package.swift)

--- UkrainianVoicesApp/App/UkrainianVoicesApp.swift
+++ UkrainianVoicesApp/App/UkrainianVoicesApp.swift
@@ -9,18 +9,14 @@ import Sentry
 @main
 struct UkrainianVoicesApp: App {
     init() {
-        // Initialize Sentry SDK for crash and hang detection
         SentrySDK.start { options in
             options.dsn = "INSERT_DSN_HERE" // Андрей добавит ключ позже
             options.enableAppHangTracking = true
-            options.appHangTimeoutInterval = 2.0 // Отлавливаем зависания дольше 2 секунд
-            options.enableCaptureFailedRequests = true
+            options.appHangTimeoutInterval = 2.0
             options.attachStacktrace = true
-            options.enableCrashHandler = true
-            options.enableAutoSessionTracking = true
         }
     }
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
