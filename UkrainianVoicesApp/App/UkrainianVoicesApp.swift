10e2990021345eef6b42a4d00e2d013f45988264
Andriy Butenko <abuten1977@gmail.com>
2026-04-18 12:40:00 +0200

    macOS: force regular activation policy

--- UkrainianVoicesApp/App/UkrainianVoicesApp.swift
+++ UkrainianVoicesApp/App/UkrainianVoicesApp.swift
@@ -5,7 +5,35 @@
 
 import SwiftUI
 #if os(macOS)
+import AppKit
 import RHVoiceKit
+#endif
+
+@MainActor
+#if os(macOS)
+private final class MacAppDelegate: NSObject, NSApplicationDelegate {
+    var isSelfTestMode = false
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        guard !isSelfTestMode else { return }
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+
+        // Best-effort: if SwiftUI already created a window, bring it forward.
+        DispatchQueue.main.async {
+            NSApp.windows.first?.makeKeyAndOrderFront(nil)
+        }
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        guard !isSelfTestMode else { return false }
+        if !flag {
+            NSApp.activate(ignoringOtherApps: true)
+            NSApp.windows.first?.makeKeyAndOrderFront(nil)
+        }
+        return true
+    }
+}
 #endif
 
 // NOTE: Sentry SDK will be added after Distribution certificate is available.
@@ -14,9 +42,13 @@ import RHVoiceKit
 @main
 struct UkrainianVoicesApp: App {
     private let isSelfTestMode = CommandLine.arguments.contains("--self-test")
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(MacAppDelegate.self) private var macAppDelegate
+    #endif
 
     init() {
         #if os(macOS)
+        macAppDelegate.isSelfTestMode = isSelfTestMode
         if isSelfTestMode {
             Task { @MainActor in
                 await RHVoiceSelfTestRunner.runAndExit()
